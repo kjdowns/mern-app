d39b8717082b0fc51b459f9dc3266509c59f7714
kjdowns <kevin.downs04@gmail.com>
2020-08-18 09:03:51 -0400

    add routes folder and files and add them to server file

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,14 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
+// Require and use Route files
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
 // Server listen
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
