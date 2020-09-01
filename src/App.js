daab89c2b9d160972d67e55d9768484971c5b07a
kjdowns <kevin.downs04@gmail.com>
2020-08-31 20:03:48 -0400

    set up initial route declarations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
 
 function App() {
   return (
-    <div className="container">
-      Hello world!
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path='/' exact component={ExercisesList} />
+      <Route path='edit/:id' component={EditExercises} />
+      <Route path='/create' component={CreateExercises} />
+      <Route path='/user' component={CreateUser} />
+    </Router>
   );
 }
 
