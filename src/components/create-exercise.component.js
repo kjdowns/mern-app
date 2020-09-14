732694b0e0da7d80bdbda569a06b815f2d84dd9f
kjdowns <kevin.downs04@gmail.com>
2020-09-14 19:42:40 -0400

    add onchangeusername method

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -14,6 +14,12 @@ export default class CreateExercises extends Component {
         }
     }
 
+    onChangeUsername(e){
+        this.setState({
+            username: e.target.value
+        })
+    }
+
     render() {
         return (
             <div>
