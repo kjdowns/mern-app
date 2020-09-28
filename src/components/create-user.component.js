9bbc854a9d35536e420e382b535eb760bc238a63
kjdowns <kevin.downs04@gmail.com>
2020-09-28 19:29:17 -0400

    add constructor

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 export default class CreateUser extends Component {
+
+    constructor(){
+        super();
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: ''
+        }
+    }
+
     render() {
         return (
             <div>
