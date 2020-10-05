bc8a2854afd299c1bf473422d2056044c9d4e7cf
kjdowns <kevin.downs04@gmail.com>
2020-10-05 17:17:31 -0400

    add eventlisteners and values to form

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -37,13 +37,13 @@ export default class CreateUser extends Component {
         return (
             <div>
                 <h3>Create New User</h3>
-                <form>
-                    <div>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
                         <label>Username: </label>
-                        <input type="text" />
+                        <input type="text" required className="form-control" value={this.state.username} onChange={this.onChangeUsername} />
                     </div>
-                    <div>
-                        <input type="submit" value="Create User"/>
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-primary"/>
                     </div>
                 </form>
             </div>
