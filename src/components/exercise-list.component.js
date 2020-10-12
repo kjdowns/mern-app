a1cdeb08ccbca7b819a0d28bd6b2305afa580eb6
kjdowns <kevin.downs04@gmail.com>
2020-10-12 19:22:49 -0400

    add componentdidmount to grab data from database

--- src/components/exercise-list.component.js
+++ src/components/exercise-list.component.js
@@ -12,6 +12,14 @@ export default class ExercisesList extends Component {
         this.state = {exercises: []};
     }
 
+    componentDidMount() {
+        axios.get('http://localhost:5000/exercises')
+            .then(response => {
+                this.setState({exercises: response.data})
+            })
+            .catch((error => console.log(error)));
+    }
+
     render() {
         return (
             <div>
