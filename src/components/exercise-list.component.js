3a9480aed37b0b6cb6b5c19670d29f19e00d5ca4
kjdowns <kevin.downs04@gmail.com>
2020-10-12 19:19:44 -0400

    add imports and class constructor

--- src/components/exercise-list.component.js
+++ src/components/exercise-list.component.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class ExercisesList extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.deleteExercise = this.deleteExercise.bind(this)
+
+        this.state = {exercises: []};
+    }
+
     render() {
         return (
             <div>
